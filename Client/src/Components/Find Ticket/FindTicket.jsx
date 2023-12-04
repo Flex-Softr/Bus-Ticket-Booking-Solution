eb436a484118dc9818217c21acb52632bcc4d0cc
NabilaWorld <mnabila30163@gmail.com>
2023-12-04 12:11:44 +0600

    correction code there

--- Client/src/Components/Find Ticket/FindTicket.jsx
+++ Client/src/Components/Find Ticket/FindTicket.jsx
@@ -244,7 +244,7 @@ function FindTicket() {
                     variant="contained"
                     className="px-6 py-2 my-3 mx-auto block"
                   >
-                    Select a Seat
+                    Select a Seat for journey
                   </Button>
                 </div>
               </div>
