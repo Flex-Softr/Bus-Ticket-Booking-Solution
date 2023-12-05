00c87be33e87cfe5096cce5009261571995f1af9
Jhimi Talukder <talukderjhimi@gmail.com>
2023-12-05 07:44:41 +0600

    add bus and dashboard sidebar done

--- Client/src/Dashboard/AddBus/AddBus.jsx
+++ Client/src/Dashboard/AddBus/AddBus.jsx
@@ -1,21 +1,28 @@
-import { useForm, Controller } from "react-hook-form";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Autocomplete from "@mui/material/Autocomplete";
-import FormControl from "@mui/material/FormControl";
-// import Typography from '@mui/material/Typography';
 
-const busTypes = ["AC", "Non-AC"];
+import { useForm, Controller } from 'react-hook-form';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Autocomplete from '@mui/material/Autocomplete';
+import FormControl from '@mui/material/FormControl';
+import Radio from '@mui/material/Radio';
+import Alert from '@mui/material/Alert';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Box from '@mui/material/Box';
+
+import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
+
+const busTypes = ['AC', 'Non-AC'];
 
 // Array of predefined locations
 const locations = [
-  "Barishal",
-  "Chattogram",
-  "Dhaka",
-  "Khulna",
-  "Rajshahi",
-  "Rangpur",
-  "Mymensingh",
+  'Barishal',
+  'Chattogram',
+  'Dhaka',
+  'Khulna',
+  'Rajshahi',
+  'Rangpur',
+  'Mymensingh',
 ];
 
 const AddBus = () => {
@@ -33,10 +40,26 @@ const AddBus = () => {
   };
 
   return (
-    <>
+    <Box sx={{ backgroundColor: '#fff', borderRadius: '3px' }}  className="px-20 py-10 md:w-11/12 mx-auto" style={{ "boxShadow": " rgba(0, 0, 0, 0.24) 0px 3px 8px" }}>
+      <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: '50%',
+        padding: '30px',
+        // color: 'white',
+        height: '40px',
+        width: '40px',
+        margin: '0 auto',
+        backgroundColor: '#d6d8da', // Add your desired background color
+      }}
+    >
+      <DirectionsBusIcon sx={{ fontSize: '44px' }} color="primary" />
+    </Box>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="p-4 w-9/12 mt-10 mx-auto"
+        className="mt-5"
       >
         <div className="flex gap-2">
           <div className="mb-4 flex-1">
@@ -47,7 +70,6 @@ const AddBus = () => {
                 required: "This field is required",
               })}
             />
-            
           </div>
           <div className="mb-4 flex-1">
             <TextField
@@ -129,31 +151,7 @@ const AddBus = () => {
           </div>
         </div>
 
-        <div className="mb-4">
-          <FormControl fullWidth>
-            <Controller
-              name="busType"
-              control={control}
-              defaultValue=""
-              rules={{ required: "this field is required" }}
-              render={({ field }) => (
-                <Autocomplete
-                  {...field}
-                  options={busTypes}
-                  freeSolo
-                  renderInput={(params) => (
-                    <TextField {...params} label="AC/Non-AC" />
-                  )}
-                  onChange={(_, value) => setValue("busType", value)}
-                />
-              )}
-            />
-            {errors.droppingPoint && (
-              <p className="text-red-600">{errors.droppingPoint.message}</p>
-            )}
-          </FormControl>
-        </div>
-
+    
         <div className="mb-4">
           <TextField
             label="Time"
@@ -163,11 +161,44 @@ const AddBus = () => {
           />
         </div>
 
+        <div className="mb-4">
+          <FormControl fullWidth>
+            <Controller
+              name="busType"
+              control={control}
+              defaultValue=""
+              rules={{ required: 'this field is required' }}
+              render={({ field }) => (
+                <RadioGroup
+                  {...field}
+                  row
+                  aria-label="busType"
+                  onChange={(e) => setValue('busType', e.target.value)}
+                >
+                  {busTypes.map((type) => (
+                    <FormControlLabel
+                      key={type}
+                      value={type}
+                      control={<Radio color="primary" />}
+                      label={type}
+                    />
+                  ))}
+                </RadioGroup>
+              )}
+            />
+            {errors.busType && (
+              // <p className="text-red-600"></p>
+              <Alert severity="warning">{errors.busType.message}</Alert>
+            )}
+          </FormControl>
+        </div>
+
+
         <Button type="submit" variant="contained" color="primary">
           Submit
         </Button>
       </form>
-    </>
+    </Box>
   );
 };
 
