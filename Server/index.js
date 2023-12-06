1b59226f574ec52cd09395cabfaac67d02c02bba
NabilaWorld <mnabila30163@gmail.com>
2023-12-06 20:16:38 +0600

    commit the correct code

--- Server/index.js
+++ Server/index.js
@@ -1,22 +1,17 @@
-const express = require('express');
-const cors = require('cors'); // Make sure this line is present
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
 const app = express();
+const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-// Use cors middleware
+
+//middle ware
 app.use(cors());
-app.use(express());
+app.use(express.json())
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2uczcxe.mongodb.net/?retryWrites=true&w=majority`;
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4zx1pf4.mongodb.net/?retryWrites=true&w=majority`;
-
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASS)
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,28 +26,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
-    // const bdDistrictsCollection=client.db('').collection('')
 
     const bdDistrictsCollection = client.db('cityTicket').collection('bdDistricts');
 
+    const supervisorDataCollection = client.db('cityTicket').collection('supervisorData');
 
     app.get('/ticket', async (req, res) => {
-      const result = await bdDistrictsCollection.find().toArray();
-      res.send(result);
-  })
+        const result = await bdDistrictsCollection.find().toArray();
+        res.send(result);
+    })
 
-  // add cart
-  app.post('/ticket', async (req, res) => {
+    // add supervisor
+
+  app.get('/supervisor', async (req, res) => {
+    const result = await supervisorDataCollection.find().toArray();
+    res.send(result);
+})
+
+// post
+app.post('/supervisor', async (req, res) => {
     const item = req.body;
-    console.log(item);
-
-    const result = await bdDistrictsCollection.insertOne(item);
-
+    console.log(item)
+    const result = await supervisorDataCollection.insertOne(item);
     res.send(result);
   })
 
-    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -65,12 +63,12 @@ async function run() {
 run().catch(console.dir);
 
 
+app.get('/', (req, res)=>{
 
+    res.send('Nabilar Chocolate House')
 
-app.get("/",(req,res)=>{
-    res.send("Bus Ticket Booking Server is running")
 })
 
-app.listen(port,()=>{
-    console.log(`Server is running on port:${port}`)
+app.listen(port, ()=>{
+    console.log(`Nabila loves chocolate ${port}`)
 })
