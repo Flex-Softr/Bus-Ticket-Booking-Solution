f5970e33f2f852e0d381497da3efab225e95f1ec
NabilaWorld <mnabila30163@gmail.com>
2023-12-25 12:11:49 +0600

    commit code

--- Client/src/Components/FixSeat/FixSeat.jsx
+++ Client/src/Components/FixSeat/FixSeat.jsx
@@ -24,47 +24,47 @@ const FixSeat = () => {
 
   const { allSeats } = useSeats();
 
-  const storedData = JSON.parse(localStorage.getItem("formData")) || {};
-  console.log("Stored Data:", storedData);
+  // const storedData = JSON.parse(localStorage.getItem("formData")) || {};
+  // console.log("Stored Data:", storedData);
 
   // Get the stored date from local storage
-  const [selectedDate, setSelectedDate] = useState(
-    storedData.departureDate || ""
-  );
+  // const [selectedDate, setSelectedDate] = useState(
+  //   storedData.departureDate || ""
+  // );
 
-  const [seatingData, setSeatingData] = useState(null);
+  // const [seatingData, setSeatingData] = useState(null);
 
-  useEffect(() => {
-    // Fetch seating data or use the provided JSON directly
-    // For example, you can fetch data from an API or use local data
-    const fetchData = async () => {
-      // Assuming the JSON is stored locally
-      const response = await fetch("../../../public/seats.json");
-      const data = await response.json();
-      setSeatingData(data);
-    };
+  // useEffect(() => {
+  //   // Fetch seating data or use the provided JSON directly
+  //   // For example, you can fetch data from an API or use local data
+  //   const fetchData = async () => {
+  //     // Assuming the JSON is stored locally
+  //     const response = await fetch("../../../public/seats.json");
+  //     const data = await response.json();
+  //     setSeatingData(data);
+  //   };
 
-    fetchData();
-  }, []); // Empty dependency array means useEffect runs only once on mount
+  //   fetchData();
+  // }, []); // Empty dependency array means useEffect runs only once on mount
 
-  const handleDateChange = (event) => {
-    setSelectedDate(event.target.value);
-  };
+  // const handleDateChange = (event) => {
+  //   setSelectedDate(event.target.value);
+  // };
 
   // Set default values for form fields
-  useEffect(() => {
-    Object.entries(storedData).forEach(([key, value]) => {
-      if (key !== "date") {
-        setValue(key, value.value);
-      }
-    });
-  }, [storedData, setValue]);
+  // useEffect(() => {
+  //   Object.entries(storedData).forEach(([key, value]) => {
+  //     if (key !== "date") {
+  //       setValue(key, value.value);
+  //     }
+  //   });
+  // }, [storedData, setValue]);
 
   // const onSubmit = (data) => {
   //   console.log(data); // You can handle form submission logic here
   // };
 
-  const [selectedSeat, setSelectedSeat] = useState(null);
+  // const [selectedSeat, setSelectedSeat] = useState(null);
 
   const onSubmit = (data) => {
     // const formDataWithSeat = {
@@ -76,23 +76,23 @@ const FixSeat = () => {
     console.log(data);
   };
 
-  useEffect(() => {
-    if (selectedSeat !== null) {
-      // Do not submit the form automatically when a seat is selected
-      // onSubmit({});
-      // Reset the selected seat to avoid duplicate submissions
-      setSelectedSeat(null);
-    }
-  }, [selectedSeat]);
+  // useEffect(() => {
+  //   if (selectedSeat !== null) {
+  //     // Do not submit the form automatically when a seat is selected
+  //     // onSubmit({});
+  //     // Reset the selected seat to avoid duplicate submissions
+  //     setSelectedSeat(null);
+  //   }
+  // }, [selectedSeat]);
 
-  const handleSeatClick = (seatId) => {
-    console.log(`Seat clicked: ${seatId}`);
-    setSelectedSeat(seatId);
-  };
+  // const handleSeatClick = (seatId) => {
+  //   console.log(`Seat clicked: ${seatId}`);
+  //   setSelectedSeat(seatId);
+  // };
 
   const thesis = useLoaderData();
 
-  const { serialNumber } = thesis;
+  const { serialNumber, pickupPoint, droppingPoint } = thesis;
 
   // seat id for changing the design after clicking
   // const handleSeatClick = (seatId) => {
@@ -107,7 +107,7 @@ const FixSeat = () => {
         // style={{ maxWidth: 400, height: 630, margin: "auto" }}
       >
         <Grid container spacing={2}>
-          <Grid item xs={12}>
+          {/* <Grid item xs={12}>
             <TextField
               className="w-full"
               type="date"
@@ -122,7 +122,7 @@ const FixSeat = () => {
             {errors.date && (
               <span className="text-red-700">This field is required</span>
             )}
-          </Grid>
+          </Grid> */}
 
           <Grid item xs={12}>
             <TextField
@@ -142,6 +142,18 @@ const FixSeat = () => {
               {...register("droppingPoint", { required: true })}
             />
             {errors.droppingPoint && (
+              <span className="text-red-700">This field is required</span>
+            )}
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              value={pickupPoint}
+              label="pickupPoint"
+              {...register("pickupPoint", { required: true })}
+            />
+            {errors.pickupPoint && (
               <span className="text-red-700">This field is required</span>
             )}
           </Grid>
