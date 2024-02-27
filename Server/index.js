0387e10eae1b5ad81c492def7ea70a3eabd2262f
manikOnline <thmanik1241@gmail.com>
2024-02-27 22:09:59 +0600

    change something

--- Server/index.js
+++ Server/index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     //  exports.bdDistrictsCollection = client
     //     .db("cityTicket")
@@ -58,7 +58,7 @@ async function run() {
     usersController.setupCollections(collections);
     // app.use
     app.use(userRouter);
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
